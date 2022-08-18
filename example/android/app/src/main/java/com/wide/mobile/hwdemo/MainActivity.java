8528c170410ac438d713e4fbfa7a9d32acb3f610
WideTechId - Mobile Team <github.mobile@primecash.co.id>
2022-08-18 14:34:51 +0700

    add latest HWCallCompact and HWPermissionManager

--- example/android/app/src/main/java/com/wide/mobile/hwdemo/MainActivity.java
+++ example/android/app/src/main/java/com/wide/mobile/hwdemo/MainActivity.java
@@ -4,12 +4,15 @@ import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.wide.mobile.hellowide.api.Constants;
+import com.wide.mobile.hellowide.api.HWConstants;
 import com.wide.mobile.hellowide.api.HWCallCompact;
+import com.wide.mobile.hellowide.api.HWPermissionManager;
 import com.wide.mobile.hellowide.api.HelloWide;
+import com.wide.mobile.hellowide.core.utils.HWPermissionCallback;
 import com.wide.mobile.hellowide.core.utils.Logger;
 import com.wide.mobile.hwdemo.firebase.MyMessagingService;
 
@@ -30,6 +33,20 @@ public class MainActivity extends AppCompatActivity {
 
         initActivity();
         Logger.d("DEVICE_TOKEN", MyMessagingService.getToken(this));
+
+        HWPermissionManager hwpm = new HWPermissionManager(this);
+        hwpm.requestPermission( new HWPermissionCallback() {
+            @Override
+            public void onCompleted(int response) {
+                Logger.d("PERMISSION TEST", String.valueOf(response));
+                if(response == HWPermissionManager.HW_OVERLAY_DENIED){
+                    finish();
+                }
+                else if(response == HWPermissionManager.HW_OVERLAY_GRANTED){
+                    Toast.makeText(context, "Permission Granted",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
         HWCallCompact.checkCallService(this);
     }
 
@@ -56,19 +73,19 @@ public class MainActivity extends AppCompatActivity {
         callButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 HelloWide helloWide = HelloWide.getInstance(context);
-                helloWide.outgoingCall(Constants.CALL_TYPE_VOICE, "Call Center");
+                helloWide.outgoingCall(HWConstants.CALL_TYPE_VOICE, "Call Center");
             }
         });
         incomingButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 HelloWide helloWide = HelloWide.getInstance(context);
-                helloWide.incomingCall(Constants.NOTIFICATION_PRESS, Constants.CALL_TYPE_VIDEO, UUID.randomUUID().toString(), "Call Center");
+                helloWide.incomingCall(HWConstants.NOTIFICATION_PRESS, HWConstants.CALL_TYPE_VIDEO, UUID.randomUUID().toString(), "Call Center");
             }
         });
         videoCallButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 HelloWide helloWide = HelloWide.getInstance(context);
-                helloWide.outgoingCall(Constants.CALL_TYPE_VIDEO, "Call Center");
+                helloWide.outgoingCall(HWConstants.CALL_TYPE_VIDEO, "Call Center");
             }
         });
     }
